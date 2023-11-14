36b267aa1a1160d582605531ad38172f6652fc29
ywang0789 <115739161+ywang0789@users.noreply.github.com>
2023-11-14 11:26:00 -0500

    added main.py for main window

---
+++ test/main.py
@@ -0,0 +1,50 @@
+# main stuff
+# only run this plz
+import sys
+
+from PyQt5.QtWidgets import QApplication, QFrame, QWidget, QVBoxLayout
+from PyQt5.QtCore import Qt, QTimer
+
+from gui.MainWidget import MainWidget
+from gui.TitleWidget import TitleWidget
+from gui.FooterWidget import FooterWidget
+
+app = QApplication(sys.argv)
+
+# window
+window = QFrame()
+window.setWindowFlags(Qt.FramelessWindowHint)
+window.setFrameStyle(QFrame.Panel | QFrame.Raised)
+window.move(0, 0)
+window.show()
+
+# layout
+layout = QVBoxLayout()
+window.setLayout(layout)
+layout.setContentsMargins(0, 0, 0, 0)
+layout.setSpacing(0)
+
+# title bar
+title = TitleWidget()
+
+
+# main widget
+main = MainWidget()
+
+# footer widget
+footer = FooterWidget()
+
+# add widgets to layout
+layout.addWidget(title)
+layout.addWidget(main)
+layout.addWidget(footer)
+
+# show
+title.show()
+main.show()
+
+
+
+
+
+sys.exit(app.exec())
