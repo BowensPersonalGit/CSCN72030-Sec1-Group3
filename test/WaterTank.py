526b26847edabdca65289ef97bbc46b4d9254bc9
ywang0789 <115739161+ywang0789@users.noreply.github.com>
2023-11-23 14:25:37 -0500

    added additional prints

--- test/WaterTank.py
+++ test/WaterTank.py
@@ -34,7 +34,9 @@ class WaterTank:
     def update(self):
         """Update values from the water monitor"""
         self.current_level = self.waterMonitor.monitorCurrentLevel()
+        print("water current level: ", self.current_level)
         self.purity = self.waterMonitor.monitorPurity()
+        print("water purity: ", self.purity)
 
 
 if __name__ == "__main__":
